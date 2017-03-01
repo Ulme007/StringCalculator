bbc0272c0daa625b2102e89f8c45033f4371c87a
utauchert <ulrich.tauchert@epost-dev.de>
2017-03-01 09:11:08 +0100

    Until number 6

--- src/main/java/com/epost/katas/stringcalculator/StringCalculator.java
+++ src/main/java/com/epost/katas/stringcalculator/StringCalculator.java
@@ -5,7 +5,7 @@ import java.util.StringTokenizer;
 
 public class StringCalculator {
 
-    public int add(String expression) {
+    public int add(String expression) throws Exception {
         if (expression.isEmpty()) {
             return 0;
         }
@@ -19,11 +19,18 @@ public class StringCalculator {
         StringTokenizer stringTokenizer = new StringTokenizer(expression, delimiter);
 
         int sum = 0;
+        String negativeNumbers = "";
         while (stringTokenizer.hasMoreTokens()) {
-            String value = stringTokenizer.nextToken();
-            int summand = Integer.parseInt(value);
-            sum = summand + sum;
+            int summand = Integer.parseInt(stringTokenizer.nextToken());
+            if (summand < 0) {
+                negativeNumbers += summand + ",";
+            } else if (summand <= 1000) {
+                sum += summand;
+            }
+        }
 
+        if (!negativeNumbers.isEmpty()) {
+            throw new Exception("Negatives not allowed: " + negativeNumbers.substring(0, negativeNumbers.length() - 1));
         }
         return sum;
     }
--- src/test/java/com/epost/katas/stringcalculator/StringCalculatorTest.java
+++ src/test/java/com/epost/katas/stringcalculator/StringCalculatorTest.java
@@ -2,7 +2,9 @@ package com.epost.katas.stringcalculator;
 
 import org.junit.Test;
 
+import static junit.framework.TestCase.fail;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 public class StringCalculatorTest {
 
@@ -53,6 +55,7 @@ public class StringCalculatorTest {
         // then
         assertEquals(3, result);
     }
+
     @Test
     public void shouldReturnSixForNumberOneAndTwoANDThree() throws Exception {
         // given
@@ -87,5 +90,91 @@ public class StringCalculatorTest {
 
         // then
         assertEquals(3, result);
+    }
+
+    @Test
+    public void shouldReturnOneForCustomDelimiterAndOne() throws Exception {
+        // given
+        StringCalculator stringCalculator = new StringCalculator();
+
+        // when
+        int result = stringCalculator.add("//;\n1");
+
+        // then
+        assertEquals(1, result);
+    }
+
+    @Test
+    public void shouldReturnZeroForCustomDelimiter() throws Exception {
+        // given
+        StringCalculator stringCalculator = new StringCalculator();
+
+        // when
+        int result = stringCalculator.add("//;\n");
+
+        // then
+        assertEquals(0, result);
+    }
+
+
+    @Test
+    public void shouldThrowExceptionAndErrorMessageWithNumbersWhenNumberNegative() throws Exception {
+        // given
+        StringCalculator stringCalculator = new StringCalculator();
+        Exception exception = null;
+
+        // when
+        try {
+            stringCalculator.add("-1");
+            fail();
+        } catch (Exception ex) {
+            exception = ex;
+        }
+
+        // then
+        assertNotNull(exception);
+        assertEquals("Negatives not allowed: -1", exception.getMessage());
+    }
+
+    @Test
+    public void shouldThrowExceptionAndErrorMessageWithTwoNumbersWhenNumbersNegative() throws Exception {
+        // given
+        StringCalculator stringCalculator = new StringCalculator();
+
+        try {
+            // when
+            stringCalculator.add("-1,-2");
+            fail();
+        } catch (Exception ex) {
+            // then
+            assertEquals("Negatives not allowed: -1,-2", ex.getMessage());
+        }
+    }
+
+    @Test
+    public void shouldThrowExceptionAndErrorMessageWithOneNegativeAndOnePositiveNumber() throws Exception {
+        // given
+        StringCalculator stringCalculator = new StringCalculator();
+
+        try {
+            // when
+            stringCalculator.add("-3,1,-2");
+            fail();
+        } catch (Exception ex) {
+            // then
+            assertEquals("Negatives not allowed: -3,-2", ex.getMessage());
+        }
+    }
+
+    @Test
+    public void shouldIgnoreNumberBiggerOT() throws Exception {
+        // given
+        StringCalculator stringCalculator = new StringCalculator();
+
+        // when
+        int result = stringCalculator.add("2,1001");
+
+        // then
+        assertEquals(2, result);
     }
 }
